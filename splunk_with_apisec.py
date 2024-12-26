90b6c2a83e019ad0d0714afadfac65c73127fd6e
kailas.bidve <kailas.bidve@artificio.ai>
2024-12-26 21:17:08 +0530

    Added admin-activity-logs API

--- splunk_with_apisec.py
+++ splunk_with_apisec.py
@@ -4,12 +4,10 @@ import os
 import logging
 from datetime import datetime, timedelta
 
-
 # Load config
 config_path = os.path.join(os.path.dirname(__file__), 'config.json')
 with open(config_path, "r") as config_file:
     config = json.load(config_file)
-
 
 # Function to retrieve bearer token by logging in
 def get_bearer_token():
@@ -18,9 +16,9 @@ def get_bearer_token():
         "username": config['auth']['username'],
         "password": config['auth']['password']
     }
-    
+
     response = requests.post(login_url, json=credentials)
-    
+
     if response.status_code == 200:
         token = response.json().get('token')
         if token:
@@ -43,9 +41,9 @@ def fetch_data(api_endpoint, params=None):
         "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json"
     }
-    
+
     response = requests.get(f"{API_BASE_URL}/{api_endpoint}", headers=headers, params=params)
-    
+
     if response.status_code == 200:
         return response.json()
     else:
@@ -138,8 +136,24 @@ def fetch_system_activity_logs():
         if paginated_data:
             print(json.dumps(paginated_data))  # Indexed as 'system_activity_logs'
 
+# Function for fetching admin activity logs
+def fetch_admin_activity_logs():
+    if config['admin_activity_logs']['enabled']:
+        # Automatically calculate the startDate and endDate
+        start_date, end_date = get_yesterday_date_range()
+
+        # Fetch admin activity logs with dynamic date range
+        params = {
+            "startDate": start_date,
+            "endDate": end_date
+        }
+        paginated_data = fetch_paginated_data("admin-activity-logs", params)
+        if paginated_data:
+            print(json.dumps(paginated_data))  # Indexed as 'admin_activity_logs'
+
 if __name__ == "__main__":
     # Fetch logs based on config
     fetch_user_activity_logs()
     fetch_login_activity_logs()
     fetch_system_activity_logs()
+    fetch_admin_activity_logs()
